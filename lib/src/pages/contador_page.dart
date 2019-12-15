12c5312581bcac1d37a50ebbd92a2f6aecc30db1
Jonhathan Rodas <jrodas4044@gmail.com>
2019-12-15 15:25:37 -0600

    Acciones

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -34,16 +34,45 @@ class _ContadorPageState extends State<ContadorPage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+      floatingActionButton: _crearBotones(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
+  }
+ 
+  Widget _crearBotones() {
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0,),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _reset,),
+        Expanded(child: SizedBox(width: 5.0,),),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _sustraer,),
+        SizedBox(width: 5.0,),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar
+        ),
+        SizedBox(width: 30.0,)
+      ],
+    );
+  }
+
+  void _agregar() {
+    setState(() {
+      _conteo++;
+    });
+  }
+
+  void _sustraer() {
+    setState(() {
+      _conteo--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _conteo = 0;
+    });
   }
 
 }
